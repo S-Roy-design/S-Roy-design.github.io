8a49f488405a5451e056564e4ff9411dcca7ef4c
Sowmma Roy <roysowmma@gmail.com>
2021-01-30 18:55:35 -0500

    My first commit
    Produced the layout, testing AJAX feature

---
+++ script.js
@@ -0,0 +1,24 @@
+//we write this function, which will hold all our code and will be called, when the page has considered itself to be fully loaded
+
+function start() {
+    // $("body").css("background-color","pink");
+
+}
+
+// Telling the browser to trigger the function-"start", after having loaded the individual sections/parts of the website 
+
+$(window).on("load", start);
+
+function loadpage(e){
+
+    e.preventDefault();
+    $("#menu a.active").removeClass("active");
+
+    //select - the document, find the ul/li that is the id #menu belongs to and add the class active to it
+    // $(document).li("#nav-link")add("#active") --- Attempt
+    $(this).addClass("active");
+    
+}
+
+//Hey browser, please listen for click on any hyperlink, if you hear it please execute the loadpage function:
+$(document).on("click", "#menu a", loadpage);
